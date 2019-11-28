e147a1be6c4f75e9a6b886e8ebcd25754ff3437e
Le Tran The Toan <letranthetoan@gmail.com>
2019-11-28 16:43:33 +0700

    implement service validation

--- app/services/transfer_service/send_money.rb
+++ app/services/transfer_service/send_money.rb
@@ -8,10 +8,15 @@ module TransferService
     def initialize(sender_id:, receiver_id:, amount: )
       @sender = User.find_by(id: sender_id)
       @receiver = User.find_by(id: receiver_id)
-      @amount = amount
+      @amount = to_big_decimal(amount)
     end
 
     def call
+      return user_not_found_err if sender.nil? | receiver.nil?
+      return amount_err unless amount && amount.positive?
+      p sender.balances.first
+      p "---------------------"
+      return user_balance_err unless sender.balances.first.amount >= amount
 
     end
 
@@ -23,6 +28,13 @@ module TransferService
       errors.add(:amount, "Số tiền chuyển không hợp lệ!")
     end
 
+    def user_not_found_err
+      errors.add(:user, 'Tài khoản người dùng không đúng!')
+    end
 
+    private
+    def to_big_decimal amount
+       BigDecimal(amount.to_s) if Float(amount) rescue nil
+    end
   end
 end
--- spec/services/transfer_service/send_money_spec.rb
+++ spec/services/transfer_service/send_money_spec.rb
@@ -2,17 +2,72 @@ require 'rails_helper'
 
 module TransferService
   RSpec.describe SendMoney do
+    let(:sender) { create(:user) }
+    let(:receiver) { create(:user) }
+    let(:sender_balance) { create(:balance, user_id: sender.id, amount: 80000) }
+    let(:receiver_balance) { create(:balance, user_id: receiver.id, amount: 0) }
 
-    context 'when sender or receiver is not valid' do
-      let(:sender) { create(:user) }
-      let(:receiver) { create(:user) }
-      let(:sender_balance) { create(:balance, user_id: sender.id, amount: 80000) }
-      let(:receiver_balance) { create(:balance, user_id: receiver.id, amount: 0) }
-      it 'succeeds' do
-        expect(sender.valid?).to be true
-        expect(receiver.valid?).to be true
+    context 'when sender is not valid' do
+      it 'will fall' do
+        result = SendMoney.call(
+          sender_id: sender.id + 100, #invalid user id
+          receiver_id: receiver.id,
+          amount: 50000
+        )
+        expect(result.success?).to eq(false)
+        expect(result.errors[:user][0]).to eq('Tài khoản người dùng không đúng!')
       end
     end
-    
+
+    context 'when receiver is not valid' do
+      it 'will fall' do
+        result = SendMoney.call(
+          sender_id: sender.id,
+          receiver_id: receiver.id + 100, #invalid user id
+          amount: 50000
+        )
+        expect(result.success?).to eq(false)
+        expect(result.errors[:user][0]).to eq('Tài khoản người dùng không đúng!')
+      end
+    end
+
+    context 'when amount is negative number' do
+      it 'will fall' do
+        result = SendMoney.call(
+          sender_id: sender.id,
+          receiver_id: receiver.id,
+          amount: -10000 #invalid amount
+        )
+        expect(result.success?).to eq(false)
+        expect(result.errors[:amount][0]).to eq('Số tiền chuyển không hợp lệ!')
+      end
+    end
+
+    context 'when amount is not a number' do
+      it 'will fall' do
+        result = SendMoney.call(
+          sender_id: sender.id,
+          receiver_id: receiver.id,
+          amount: '12asd123afs' #invalid amount
+        )
+        expect(result.success?).to eq(false)
+        expect(result.errors[:amount][0]).to eq('Số tiền chuyển không hợp lệ!')
+      end
+    end
+
+    context "when sender's balance is less than amount" do
+      it 'will fall' do
+        sender_balance # invoke user's balance
+        receiver_balance # invoke user's balance
+        result = SendMoney.call(
+          sender_id: sender.id,
+          receiver_id: receiver.id,
+          amount: 100000 #invalid amount ( 100k > 80k )
+        )
+        expect(result.success?).to eq(false)
+        expect(result.errors[:balance][0]).to eq('Số dư tài khoản không đủ!')
+      end
+    end
+
   end
 end
