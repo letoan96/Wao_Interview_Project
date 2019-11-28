df36a610f944545868f1dba9765af0f6fa77a6b0
Le Tran The Toan <letranthetoan@gmail.com>
2019-11-28 17:07:37 +0700

    implement send money transaction

--- app/services/transfer_service/send_money.rb
+++ app/services/transfer_service/send_money.rb
@@ -6,18 +6,30 @@ module TransferService
                 :amount
 
     def initialize(sender_id:, receiver_id:, amount: )
+      # accept that we can pass in user objects. However, I prefer using sender_id and receiver_id in this case for better illustration
       @sender = User.find_by(id: sender_id)
       @receiver = User.find_by(id: receiver_id)
-      @amount = to_big_decimal(amount)
+      @amount = to_big_decimal(amount) #  return 'nil' if amount is not a number nor is positive
     end
 
     def call
       return user_not_found_err if sender.nil? | receiver.nil?
       return amount_err unless amount && amount.positive?
-      p sender.balances.first
-      p "---------------------"
-      return user_balance_err unless sender.balances.first.amount >= amount
+      return user_balance_err unless amount < sender.balances.first.amount
 
+      Balance.transaction do
+        sender_balance = sender.balances.first
+        sender_balance.with_lock do
+          sender_balance.amount -= amount
+          sender_balance.save!
+        end
+
+        receiver_balance = receiver.balances.first
+        receiver_balance.with_lock do
+          receiver_balance.amount += amount
+          receiver_balance.save!
+        end
+      end
     end
 
     def user_balance_err
